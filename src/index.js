f38d6ea9435b81e18a856b8edcb20ae91e414b28
fish895623 <dan990429@gmail.com>
2021-07-19 11:37:31 +0900

    use route to print hello

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3001
+
 const bodyParser = require("body-parser")
-
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-  console.log(req)
-  res.send('Hello World!')
-})
+app.route('/')
+    .get(function (req, res) {
+        res.send('Hello World!')
+    })
 
 app.route('/bb')
-  .get(function (_, res) {
-    res.send(`Get Random`)
-  })
-  .post(function (req, res)  {
-    console.log(`${req.body}`)
-    // res.send(`${req.body.sentence}`)
-    res.send(req.body)
-  })
+    .get(function (_, res) {
+        res.send(`Get Random`)
+    })
+    .post(function (req, res) {
+        res.send(req.body)
+    })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://192.168.0.16:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
